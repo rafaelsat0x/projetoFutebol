0cbf9b847a27a81271594c2a3f164833c64c1472
Luiz E. Dias <101161724+dsluizdias@users.noreply.github.com>
2025-06-03 17:07:47 -0300

    Update trabajo.py
    Now we have lists with the teams and yours respective state championships

--- trabajo.py
+++ trabajo.py
@@ -1,3 +1,53 @@
-matrix = [[2,4],[3,6]]
-for l,c in matrix:
-	print(l * 2, c * 2)
+Paranaense = ["Athletico-PR", "Coritiba", "Londrina", "Maringá", "Operário-PR", "Azuriz", "Cianorte", "FC Cascavel", "Independente São Joseense", "PSTC", "São Joseense", "Andraus", "Paraná Clube"]
+
+Catarinense = ["Criciúma", "Avaí", "Chapecoense", "Figueirense", "Brusque", "Marcílio Dias", "Barra", "Joinville", "Concordia", "Hercílio Luz", "Nação", "Inter de Lages"]
+
+Gaúcho = ["Grêmio", "Internacional", "Juventude", "Caxias", "Brasil de Pelotas", "Novo Hamburgo", "São José-RS", "Ypiranga", "Avenida", "Guarany de Bagé", "Santa Cruz-RS", "São Luiz"]
+
+Paulista = ["Corinthians", "Palmeiras", "São Paulo", "Santos", "Botafogo-SP", "Ponte Preta", "Novorizontino", "Ituano", "Mirassol", "Santo André", "Água Santa", "Inter de Limeira", "Guarani", "Portuguesa", "Red Bull Bragantino", "São Bernardo"]
+
+Carioca = ["Flamengo", "Fluminense", "Vasco da Gama", "Botafogo", "Nova Iguaçu", "Boavista", "Madureira", "Portuguesa-RJ", "Audax Rio", "Bangu", "Sampaio Corrêa-RJ", "Volta Redonda"]
+
+Mineiro = ["Atlético-MG", "Cruzeiro", "América-MG", "Athletic Club", "Tombense", "Uberlândia", "Villa Nova", "Pouso Alegre", "Patrocinense", "Democrata GV", "Itabirito", "Caldense"]
+
+Capixaba = ["Rio Branco-ES", "Vitória-ES", "Desportiva Ferroviária", "Serra", "Real Noroeste", "Nova Venécia", "Águia Branca", "Jaguaré", "Porto Vitória", "Estrela do Norte"]
+
+Baiano = ["Bahia", "Vitória", "Jacuipense", "Atlético de Alagoinhas", "Barcelona de Ilhéus", "Bahia de Feira", "Jequié", "Itabuna", "Jacobina", "Juazeirense"]
+
+Pernambucano = ["Sport", "Náutico", "Santa Cruz", "Retrô", "Petrolina", "Afogados", "Maguary", "Central", "Porto", "Salgueiro"]
+
+Cearense = ["Fortaleza", "Ceará", "Ferroviário", "Floresta", "Iguatu", "Maracanã", "Barbalha", "Caucaia", "Atlético Cearense", "Pacajus"]
+
+Alagoano = ["CRB", "CSA", "ASA", "Murici", "Penedense", "Coruripe", "Cruzeiro-AL", "CSE"]
+
+Potiguar = ["ABC", "América-RN", "Santa Cruz de Natal", "Potiguar de Mossoró", "Alecrim", "Força e Luz", "Globo FC", "Baraúnas"]
+
+Paraibano = ["Botafogo-PB", "Treze", "Campinense", "Nacional de Patos", "Sousa", "CSP", "Serra Branca", "São Paulo Crystal", "Pombal", "Atlético Cajazeirense"]
+
+Sergipano = ["Confiança", "Itabaiana", " Sergipe", "Dorense", "Lagarto", "Frei Paulistano", "Carmópolis", "Atlético Gloriense", "Falcon", "Olímpico"]
+
+Piauiense = ["Altos", "River-PI", "Parnahyba", "Fluminense-PI", "Oeirense", "4 de Julho", "Caiçara", "Corisabbá"]
+
+Maranhense = ["Sampaio Corrêa", "Moto Club", "Maranhão", "Imperatriz", "Pinheiro", "Chapadinha", "Cordino", "Tuntum"]
+
+Goiano = ["Goiás", "Atlético-GO", "Vila Nova", "Anápolis", "Aparecidense", "Goianésia", "Jataiense", "CRAC", "Goiatuba", "Iporá", "Morrinhos", "Grêmio Anápolis"]
+
+Mato_Grossense = ["Cuiabá", "União Rondonópolis", "Luverdense", "Nova Mutum", "Mixto", "Dom Bosco", "Academia", "Cacerense", "Primavera", "Araguaia"]
+
+Sul_Mato_Grossense = ["Operário-MS", "Portuguesa-MS", "Corumbaense", "Aquidauanense", "Dourados", "Costa Rica-MS", "Novo FC", "Coxim", "Ivinhema", "Náutico-MS"]
+
+Brasiliense = ["Brasiliense", "Capital", "Ceilândia", "Gama", "Paranoá", "Samambaia", "Santa Maria", "Taguatinga", "Real Brasília", "Unaí"]
+
+Amazonense = ["Manaus", "Amazonas", "Nacional-AM", "São Raimundo-AM", "Princesa do Solimões", "Manauara", "Parintins", "Rio Negro-AM", "Iranduba", "Unidos do Alvorada"]
+
+Paraense = ["Remo", "Paysandu", "Águia de Marabá", "Tuna Luso", "Castanhal", "Caeté", "Cametá", "Bragantino-PA", "Santa Rosa", "Tapajós", "Canaã", "Parauapebas"]
+
+Acreano = ["Atlético Acreano", "Rio Branco-AC", "Humaitá", "Galvez", "Andirá", "ADESG", "Plácido de Castro", "Náuas", "São Francisco-AC", "Vasco-AC"]
+
+Rondoniense = ["Porto Velho", "Ji-Paraná", "Real Ariquemes", "Genus", "União Cacoalense", "Vilhenense", "Barcelona-RO", "Guaporé"]
+
+Tocantinense = ["Tocantinópolis", "Capital-TO", "Araguacema", "União Carmolândia", "Gurupi", "Batalhão", "Tocantins de Miracema", "Bella Vista"]
+
+Amapaense = ["Independente-AP", "Trem", "Santos-AP", "Ypiranga-AP", "Macapá", "Santana", "São Paulo-AP", "Oratório"]
+
+Roraimense = ["Atlético Roraima", "São Raimundo-RR", "Náutico-RR", "River-RR", "Baré", "Gavião", "GAS", "Rio Negro-RR"]
